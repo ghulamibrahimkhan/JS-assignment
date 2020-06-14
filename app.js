33f845b29638587442e86f2fcd16c0fa5d15e88e
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-06-14 15:56:53 +0500

    uploading

---
+++ app.js
@@ -0,0 +1,138 @@
+        // *** Chapter # 1 ***//
+
+    // (Task 1)
+// alert("Error! Please enter a valid password");
+
+    // (Task 2)
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+    // (Task 3)
+// alert("Welcome to JS Land");
+// alert("Happy Coding!");
+
+    // (Task 4) 
+// sir yr console my save hi nahi  ho raha, refresh ho k gyab ho raha hy
+// alert("Hello..I can run JS through my web browser's console");
+
+//  *************************************************************** //
+
+        // *** Chapter # 2 ***//
+
+    // (Task 1)
+// var userName;
+
+    // (Task 2)
+// var myName = "Ghulam Ibrahim Khan";
+
+    // (Task 3)
+// var message;
+// message = "Hello World"
+// alert(message);
+
+//     // (Task 4)
+// var yourName = prompt("Enter Your Name","Ghulam Ibrahim Khan");
+// var yourAge = +prompt("Enter Your Age","20");
+// var courseInroll = prompt("Course you're inroll in","Certified Mobile Application Development");
+// alert(yourName);
+// alert(yourAge);
+// alert(courseInroll);
+
+    // (Task 5)
+// var pza = "pizza\npizz\npiz\npi\np";
+// var pza = pza.toUpperCase();
+// alert(pza)
+
+    // (Task 6)
+// var email = "gulamibrahimkhan@gmail.com";
+// alert("My email adress is " + email)
+
+    // (Task 7)
+// var book = "A smarter way to learn JavaScript ";
+// alert("I'm trying to learn from a book " + book);
+
+    // (Task 8)
+// document.write("Yah ! I can write HTML content through JavaScript")
+
+    // (Task 9)
+// var ajeeb = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(ajeeb);
+
+//  *************************************************************** //
+
+        // *** Chapter # 4 ***//
+
+    // (Task 1)
+// var age = 20;
+// alert("I'm " + age + " years old");
+
+    // (Task 2)
+// var visits = 7;
+// alert("You have visited this site " + visits + " times" );
+;
+    // (Task 3)
+// var birthYear = 2000;
+// document.write("My birth year is " + birthYear + "<br>" + "Data type of my decleared variable is number");
+
+//      // (Task 4)
+// var visitorName= "John Doe";
+// var productTitle = "T-shirt(s)";
+// var quantity = 5;
+// document.write(visitorName + "ordered " + quantity + " " +productTitle + " on XYZ clothing store");
+
+//  *************************************************************** //
+
+        // *** Chapter # 4 ***//
+
+    // (Task 1)
+// var one; var two; var three;
+
+    // (Task 2)
+//legal\\
+// var one
+// var two2
+// var three3
+// var _four4
+// var $five5
+
+//illegal\\
+// var 1;
+// var !two;
+// var &three;
+// var var;
+// var alert;
+
+    // (Task 3)
+// document.write("<h1>" +"Rules for naming JS variables" + "<h1>" + "<br>");
+// document.write("<h4>" + "Variable names can only contain , numbers,$ or _. For example : $my_1stVariable" +
+//  "<br>" + "Variables must begin with a letter, $ or _.For example: $name, _name or name" + "<br>" + 
+//  "Variable names are case sensentive" + "<br>" + 
+// "Variable names should not be JS keywords" + "<h4>");
+
+//  *************************************************************** //
+
+        // *** Chapter # 5 ***//
+
+//     // (Task 1)
+// var newVar = 3 + 5;
+// document.write(newVar + "<br>");
+
+//     // (Task 2.1)
+// var newVarsSub = 3 - 5;
+// document.write(newVarsSub + "<br>");
+
+//     // (Task 2.2)
+// var newVarDiv = 3 / 5;
+// document.write(newVarDiv + "<br>");
+
+//     // (Task 2.3)
+// var newVarMul = 3 * 5;
+// document.write(newVarMul + "<br>");
+
+//     // (Task 2.4)
+// var newVarMod = 3 % 5;
+// document.write(newVarMod + "<br>");
+
+    // (Task 3)
+var varDec;
+
+    
