58d16340dc6f0358e9fdf1b7b2b9069cd3dc16dd
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-06-16 12:46:48 +0500

    chapter 12 to 13 update 3

--- app.js
+++ app.js
@@ -583,3 +583,23 @@
 // }
 
    // (Task 7)
+// var time = prompt("Enter Time like '1900'= (7pm) ")
+// if(time >= 0000 && time <= 1200){
+//     document.write("Good Morning !")
+// }
+// else if(time >= 1200 && time <= 1700){
+//     document.write("Good Afternoon !");
+// }
+// else if(time >= 1700 && time <= 2100 ){
+//     document.write("Good Evening !");
+// }
+// else if(time >= 2100 && time <= 2359){
+//     document.write("Good Night !");
+// }
+// else{
+//     document.write("you have entered worng time");
+// }
+
+//  *************************************************************** //
+
+        // *** Chapter # 12 to 13 ***//
